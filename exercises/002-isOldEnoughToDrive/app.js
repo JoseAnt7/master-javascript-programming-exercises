b04babe0851a121b728ded7012121d92c838407d
José Antonio Llorens Padilla <131461633+JoseAnt7@users.noreply.github.com>
2024-07-12 17:49:30 +0000

    progreso

--- exercises/002-isOldEnoughToDrive/app.js
+++ exercises/002-isOldEnoughToDrive/app.js
@@ -1,4 +1,10 @@
 function isOldEnoughToDrive(age) {
-  // your code here
-  
+  if (age < 16){
+    return false;
+  }else {
+    return true;
+  }
 }
+
+let output = isOldEnoughToDrive(22);
+console.log(output);
--- exercises/006-getFullName/app.js
+++ exercises/006-getFullName/app.js
@@ -1,4 +1,8 @@
 function getFullName(firstName, lastName) {
   // your code here
-  
+  return firstName +" "+lastName;
 }
+
+let name = getFullName("Jose Antonio", "Llorens");
+
+console.log(name);
