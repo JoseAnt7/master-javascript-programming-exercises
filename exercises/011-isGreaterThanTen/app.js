a33258f3fafb24c45e8f24f70a42249cc125d546
José Antonio Llorens Padilla <131461633+JoseAnt7@users.noreply.github.com>
2024-07-16 11:47:40 +0000

    .

--- exercises/011-isGreaterThanTen/app.js
+++ exercises/011-isGreaterThanTen/app.js
@@ -1,4 +1,11 @@
 function isGreaterThanTen(num) {
-  // your code here
+  if (num > 10){
+    return true;
+  }else{
+    return false;
+  }
   
 }
+
+let salida = isGreaterThanTen(25);
+console.log(salida);
