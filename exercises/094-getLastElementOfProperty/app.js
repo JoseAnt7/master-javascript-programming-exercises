e61cb774116bd06408f2e171d1b18dcf00ca28f2
José Antonio Llorens Padilla <131461633+JoseAnt7@users.noreply.github.com>
2024-07-31 16:39:42 +0000

    completo

--- exercises/094-getLastElementOfProperty/app.js
+++ exercises/094-getLastElementOfProperty/app.js
@@ -1,1 +1,15 @@
 // Write your function here
+function getLastElementOfProperty(obj,key) {
+
+    if(!obj[key] || !Array.isArray(obj[key])){
+        return undefined;
+    }
+    let number = obj[key].length;
+    return obj[key][number -1];
+}
+
+let obj = {
+    key: [1, 2, 5]
+  };
+  let output = getLastElementOfProperty(obj, 'key');
+  console.log(output); // --> 5
